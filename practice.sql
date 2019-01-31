eec21982aa7a52c2353152cc665ef562ec67987a
Gray754 <lgc9312@gmail.com>
2019-01-31 07:28:43 -0600

    finished afternoon assignment

--- practice.sql
+++ practice.sql
@@ -77,3 +77,101 @@ where albumId in
 (select artistId from artist where name = 'Queen')
 )
 
+update customer
+set fax = null
+where fax IS NOT null
+
+update customer
+set company = 'Self'
+where company IS null
+
+update customer
+set last_name = 'Thompson'
+where first_name = 'Julia' AND last_name = 'Barnett'
+
+update customer
+set support_rep_id = 4
+where email = 'luisrojas@yahoo.cl'
+
+update track
+set composer = 'The Darkness Around Us'
+where genre_id in (select genre_id from genre 
+                   where name = 'Metal')
+and composer IS null
+
+select count(*), g.name
+from track t
+join genre g on t.genre_id = g.genre_id
+group by g.name
+
+select count(*), g.name
+from track t
+join genre g on t.genre_id = g.genre_id
+where g.name = 'Pop' OR g.name = 'Rock'
+group by g.name
+
+select ar.name, count(*)
+from album al
+join artist ar on ar.artist_id = al.album_id
+group by ar.name
+
+select distinct composer
+from track
+
+select distinct billing_postal_code
+from invoice
+
+select distinct company
+from customer
+
+delete from practice_delete
+where type = 'bronze'
+
+delete from practice_delete
+where type = 'silver'
+
+delete from practice_delete
+where value = 150
+
+create table user (
+  user_id SERIAL PRIMARY KEY,
+  name text,
+  email text
+)
+
+create table product (
+  product_id SERIAL PRIMARY KEY,
+  name text,
+  price int
+)
+
+create table product_order (
+  order_id SERIAL PRIMARY KEY
+)
+
+alter table product_order
+FOREIGN KEY (product_id) REFERENCES product (product_id)
+
+alter table product
+FOREIGN KEY (user_id) REFERENCES user (user_id)
+
+insert into users (name, email)
+values ('bob', 'bob.com'),
+('billy', 'billy.com'),
+('jack', 'jack.com')
+
+insert into product (user_id, name, price)
+values (1, 'egg', 2.99),
+(2, 'pogo stick', 12.99),
+(3, 'pack of gum', 1.99)
+
+select * from product
+limit 1
+
+select * from product_order po
+join product p on
+p.product_id = po.product_id
+
+select SUM(*) from product_order po
+join product p on
+p.product_id = po.product_id
